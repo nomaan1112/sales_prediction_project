0cd0d605a01107492a9eaa15c09fcb0cc5ac8d92
unknown <nomaanansarii100@gmail.com>
2022-07-20 15:26:36 +0530

    Logger and Exception tested successfully

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sales_prediction_project
+
+docker image: 3690d63a9180
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+from tkinter import E
+from sales.logger import logging
+from sales.exception import SalesException
 from flask import Flask
-import sys
+import sys,os
 
 app= Flask(__name__)
 
 @app.route("/", methods =['GET','POST'])
 
 def index():
-    return "Store Sales Prediction."
+    try:
+        pass
+    except Exception as e:
+        raise SalesException (e,sys) from E    
 
 
 if __name__ == "__main__":
