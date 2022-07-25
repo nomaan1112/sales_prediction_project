cff8d7b83ed12ac6f32db4a1a53d878e08ae7a92
unknown <nomaanansarii100@gmail.com>
2022-07-25 13:26:14 +0530

    Pipelines tested Successfully

--- sales/entity/config_entity.py
+++ sales/entity/config_entity.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 
 DataIngestionConfig=namedtuple("DataIngestionConfig",
-["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+["ingested_train_dir","ingested_test_dir"])
 
 
 
